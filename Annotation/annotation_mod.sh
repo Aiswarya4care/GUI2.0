ad3b9cac87921e6175c926f7c2f67fba787a0086
Aiswarya4care <aiswarya@4basecare.com>
2022-03-17 07:30:42 +0530

    update till annotation

---
+++ Annotation/annotation_mod.sh
@@ -0,0 +1,41 @@
+input="list.txt"
+while IFS= read -r line
+do
+mkdir ${line}
+echo "Starting ${line}"
+#copying.hard-filtered.vcf from basespace and unzipping
+echo "##########Starting copying.hard-filtered.vcf from basespace and unzipping#################"
+cp {{projectdir}}/AppResults/${line}/Files/${line}.hard-filtered.vcf.gz /home/basecare/Annotation_SPK
+gunzip ${line}.hard-filtered.vcf.gz
+echo "##########Ending copying.hard-filtered.vcf from basespace and unzipping#################"
+
+#finding depth
+echo "##########Starting finding depth#################"
+bedtools bamtobed -i {{projectdir}}/AppResults/${line}/Files/${line}.bam | bedtools coverage -header -a ${line}.hard-filtered.vcf -b - | sed '/^##/d' > ${line}/${line}.tsv
+##modifying the vcf with depth(field)
+awk 'NR==1{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"Overlaps_from_BAM"$12 ;next}{print}' ${line}/${line}.tsv > ${line}/${line}_mod.tsv
+echo "##########Ending finding depth#################"
+
+#vcf to table
+echo "##########Starting.hard-filtered.vcf conversion to tab#################"
+python3 /home/basecare/Programs/VCF-Simplify-master/VcfSimplify.py SimplifyVCF -toType table -inVCF "${line}.hard-filtered.vcf" -outFile "${line}/${line}.tab"
+
+##merging the tab file and tsv file 
+paste ${line}/${line}.tab ${line}/${line}_mod.tsv | cut -f 1-24,35 > ${line}/${line}_final.tab
+
+echo "##########Ending.hard-filtered.vcf conversion to tab#################"
+#annovar
+echo "##########Starting annovar annotation#################"
+perl /home/basecare/Programs/Annotation_db/convert2annovar.pl -format vcf4old "${line}.hard-filtered.vcf" > "${line}/${line}.avinput"
+perl /home/basecare/Programs/Annotation_db/table_annovar.pl "${line}/${line}.avinput" /home/basecare/Programs/Annotation_db/humandb/ -buildver hg19 -out "${line}/${line}_out" -remove -protocol ensGene,avsnp150,clinvar_20190305,intervar_20170202,intervar_20180118,esp6500siv2_all,exac03,gnomad211_exome,1000g2015aug_all,1000g2015aug_SAS,cadd13gt20,dbnsfp35a,dbscsnv11,dbnsfp31a_interpro -operation g,f,f,f,f,f,f,f,f,f,f,f,f,f -nastring .
+echo "##########Ending annovar annotation#################"
+#preparing config file
+echo "##########Starting cancervar annotation#################"
+perl /home/basecare/Annotation_SPK/config.pl ${line}/${line}
+#running cancervar
+python3 /home/basecare/Programs/Annotation_db/CancerVar.py -c config.ini
+echo "##########Ending cancervar annotation#################"
+rm config.ini
+done < "$input"
+echo "################## ALL FILES ARE DONE ###########################"
+
--- main.py
+++ main.py
@@ -3,17 +3,25 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import *
+import subprocess
+
 from cutadapt_fqc import cafa
 from cutadapt_fqc_drag import cafadra
+from anno_som import anno
 
 window=tk.Tk()
 
-# declaring string variab nle
+global GUIpath
+GUIpath=os.getcwd()
+
+# declaring string variables for tkinter
 library_kit=tk.StringVar()
 location_name=tk.StringVar()
 project= tk.StringVar()
 folderPath=tk.StringVar()
 appsession=tk.StringVar()
+projectdirPath=tk.StringVar()
 
 #setting the windows size
 window.geometry("650x600")
@@ -27,6 +35,11 @@ def browse():
     global appsession
     folder_selected = filedialog.askdirectory()
     folderPath.set(folder_selected)
+
+def projectdir_browse():
+    #choosing a project dir
+    projectdir_selected = filedialog.askdirectory()
+    projectdirPath.set(projectdir_selected)
     
 
 def globalva_update():
@@ -34,13 +47,18 @@ def globalva_update():
     proj= project.get()
     location = folderPath.get()
     appsess= appsession.get()
-    file1 = open('/home/ash/Documents/GitHub/GUI2.0/globalv.py',"w+")
-    file1.write('location=' + "'" + location + "'")
-    file1.write('\n' + 'libkit=' + "'" + libkit + "'")
-    file1.write('\n' +'proj=' + "'" + proj + "'")
-    file1.write('\n' +'appsess=' + "'" + appsess + "'")
+    projectdir=projectdirPath.get()
+    file1 = open(GUIpath + '/globalv.py',"w+")
+    l1= 'location=' + "'" + location + "'"
+    l2= 'libkit=' + "'" + libkit + "'"
+    l3= 'proj=' + "'" + proj + "'"
+    l4= 'proj=' + "'" + proj + "'"
+    l5='appsess=' + "'" + appsess + "'"
+    l6='projectdir=' + "'" + projectdir + "'"
+    file1.writelines([l1,'\n',l2,'\n',l3,'\n',l4,'\n',l5,'\n',l6])
     file1.close()
-    print('###### Done ########')
+    display= l1 + '\n' +l2+'\n'+l3+'\n'+l4+'\n'+l5+'\n'+l6
+    Output.insert(END, display)
 
 #Run Cutadapt and FastQC
 def cutad_fqc():
@@ -50,6 +68,22 @@ def cutad_fqc():
 def cutad_fqc_dra():
     cafadra()
 
+############################# Annotation & Filtration (Somatic & Germline) #################################
+def annotate():
+    anno()
+
+
+def quit():
+    file1 = open(GUIpath + '/globalv.py',"w+")
+    file1.write('location=' + "'" + "'")
+    file1.write('\n' + 'libkit=' + "'"  + "'")
+    file1.write('\n' +'proj=' + "'" + "'")
+    file1.write('\n' +'appsess=' + "'"  + "'")
+    file1.write('\n' +'projectdir=' + "'"  + "'")
+    file1.close()
+    window.destroy()
+
+
 
 ################################################
 ############# Button ###########################
@@ -57,62 +91,75 @@ def cutad_fqc_dra():
 
 
 #browse button
-samplelabel= tk.Label(window, text='Choose folder path')
+browselabel= tk.Label(window, text='Choose folder path')
 browse_btn=tk.Button(window,text = 'Browse', command = browse, height = 1, width = 18)
-samplelabel.config(font=('Nunito Sans',13))
+browselabel.config(font=('Nunito Sans',12))
 
 
 #dropdown for library kits    
 kitlabel= tk.Label(window, text= 'Select the library kit')
-kitchoosen = ttk.Combobox(window, width = 27, textvariable = library_kit)
+kitchoosen = ttk.Combobox(window, width = 26, textvariable = library_kit)
 kitchoosen['values'] = ('Roche', 'Illumina','Agilent')
-kitlabel.config(font=('Nunito Sans',13))
+kitlabel.config(font=('Nunito Sans',12))
 
 #selecting projects and retrieving project IDs
 projectlabel= tk.Label(window, text= 'Select the project')
-projectchoosen = ttk.Combobox(window, width = 27, textvariable = project)
+projectchoosen = ttk.Combobox(window, width = 26, textvariable = project)
 projectchoosen['values'] = ('Somatic DNA', 'Somatic RNA','Germline')
-projectlabel.config(font=('Nunito Sans',13))
+projectlabel.config(font=('Nunito Sans',12))
+
+#Appsession details
+appsessionlabel= tk.Label(window, text='Enter Appsession details')
+appsession= tk.Entry(window, width = 27, textvariable=appsession)
+appsessionlabel.config(font=('Nunito Sans',12))
 
 #submit button
-cafa_btn=tk.Button(window,text = 'Submit', command = globalva_update, height = 1, width = 22)
+globalv_btn=tk.Button(window,text = 'Submit', command = globalva_update, height = 1, width = 15)
 
+#Display Information
+Output = Text(window, height = 7, width = 60, bg = "light cyan")
 
 #Cutadapt & FastQ button
 cutad_fqclabel= tk.Label(window, text='Cutadapt & FastQC')
-cutad_fqc_btn=tk.Button(window,text = 'Cutadapt & FastQ', command = cutad_fqc, height = 1, width = 18)
+cutad_fqc_btn=tk.Button(window,text = 'Cutadapt & FastQ', command = cutad_fqc, height = 1, width = 16)
 cutad_fqclabel.config(font=('Nunito Sans',13))
 
+#Cutadapt, Fastqc and dragen button
+cutad_fqc_dra_btn=tk.Button(window,text = 'Run FQ, CA and Dragen', command = cutad_fqc_dra, height = 1, width = 16)
 
-#Dragen Run details
-appsessionlabel= tk.Label(window, text='Enter Appsession details')
-appsession= tk.Entry(window, width = 27, textvariable=appsession)
-appsessionlabel.config(font=('Nunito Sans',13))
 
-#Run all at once button
-cutad_fqc_dra_btn=tk.Button(window,text = 'Run FQ, CA and Dragen', command = cutad_fqc_dra, height = 1, width = 22)
+#projectdir browse button
+projlabel= tk.Label(window, text='Choose Project Folder')
+projbrowse_btn=tk.Button(window,text = 'Select Project Folder', command = projectdir_browse, height = 1, width = 18)
+projlabel.config(font=('Nunito Sans',12))
+
+#Annotation_Somatic
+annotate_btn=tk.Button(window, text = 'Annotation', command = annotate,height = 1, width = 15) 
 
 #Quit button
-close_btn=tk.Button(window, text="Quit", command=window.destroy, height = 1, width = 22)
+close_btn=tk.Button(window, text="Quit", command=quit, height = 1, width = 15)
 
 
 ################# Positioning ##########################
-cutad_fqc_btn.grid(row=5, column=0,pady=3)
-samplelabel.grid(row=0,column=0,pady=3)
+browselabel.grid(row=0,column=0,pady=3)
 browse_btn.grid(row=0, column=1,pady=3)
-cafa_btn.grid(row=4,column=0,pady=3)
-
-appsessionlabel.grid(row=3,column=0,pady=3)
-appsession.grid(row=3,column=1,pady=3)
-cutad_fqc_dra_btn.grid(row=4,column=1,pady=3)
-
-close_btn.grid(row=12,column=0,pady=3)
 kitlabel.grid(row=1, column=0,pady=3)
 kitchoosen.grid( row = 1,column = 1,pady=3)
 projectlabel.grid(row=2,column=0,pady=3)
 projectchoosen.grid(row=2, column=1,pady=3)
+appsessionlabel.grid(row=3,column=0,pady=3)
+appsession.grid(row=3,column=1,pady=3)
+projlabel.grid(row=4,column=0,pady=3)
+projbrowse_btn.grid(row=4, column=1,pady=3)
+globalv_btn.grid(row=5,column=0,pady=3)
+Output.grid(row=6,column=0,pady=3,columnspan=5)
+cutad_fqc_btn.grid(row=8, column=0,pady=3)
+cutad_fqc_dra_btn.grid(row=8,column=1,pady=3)
 
 
+annotate_btn.grid(row=9,column=0,pady=3) 
+
+close_btn.grid(row=12,column=0,pady=3)
 
 window.mainloop()        
   
