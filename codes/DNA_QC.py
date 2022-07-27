c8f91d2bfaa739ec2317b0a9be35b8251355d658
Aiswarya4care <aiswarya@4basecare.com>
2022-07-27 17:27:48 +0530

    dna qc fix

--- codes/DNA_QC.py
+++ codes/DNA_QC.py
@@ -22,7 +22,8 @@ def dna_qc():
     samples=np.array(samples).tolist()
 
     #Removing default file names from the sample name list
-    default_files=config_gui.default_files
+    default_files=['ca','panel', 'ca_fq_dragen36.sh','dragen39.sh','cutadaptlog.txt','FQlog.txt','MSI','CNV','run_cnv.sh','cutadaptlog', 'QC','FE','tmbmerged','tmbfiltered','tmb','sample','config.pl','annotation'] #pre-existing file names to be removed from the sample name list
+
     for s in default_files:
         if s in samples:
             samples.remove(s)
@@ -32,24 +33,14 @@ def dna_qc():
     os.system('mkdir '+ location + '/QC')
 
     ###### fetching sample names #########3
-    samples=glob.glob(location+"/*.html")
-
-    #renaming the files (removing _S1_L001_R1_001_)
-    for i in samples:
-        if '_S1_L001_R1_001_fastqc.html' in i:
-            os.rename(i,i.split('_S1_L001_R1_001_fastqc.html')[0]+".html")
+    samples=glob.glob(location+"/*S1_L001_R1_001_fastqc.html")
 
     #segregating the files to respective folder
     se8=list(filter(lambda x:'-SE8-' in x, samples))
     ce=list(filter(lambda x:'-CE-' in x, samples))
 
-    #choosing between germline and somatic
-    if sample_type=='DNA [Blood]':
-        script_path= GUIpath + '/dna_qc_scripts/germline/'
-        
-    else:
-        script_path= GUIpath + '/dna_qc_scripts/somatic/'
-            
+    script_path= GUIpath + '/dna_qc_scripts/somatic/'
+    
     ############# performing QC based on SE8 #######################
     if len(se8)>0:
         os.system('mkdir '+ location + '/QC/SE8')
@@ -58,16 +49,23 @@ def dna_qc():
         #copying script inside the QC folder
         for script in glob.glob(script_path+'*SE8.py'):
             shutil.copy(script,location + '/QC/SE8')
+        
+        #copying html files to the respective folder and renaming them
         for s in se8:
             shutil.copy(s,location + '/QC/SE8')
         
+        #renaming the files (removing _S1_L001_R1_001_)
+        for i in os.listdir(location+'/QC/SE8/'):
+            if '_S1_L001_R1_001_fastqc.html' in i:
+                os.rename(i,i.split('_S1_L001_R1_001_fastqc.html')[0]+".html")
+                
         os.chdir(location+'/QC/SE8')
         
         #adding location to the python script
         with open(glob.glob(location+"/QC/SE8/*.py")[0], 'r') as file :
             filedata = file.read()
             # Replace the location
-            filedata = filedata.replace('{{location}}', "'" +location + "'")
+            filedata = filedata.replace('{{location}}', "'"+location+"'")
         with open(glob.glob(location+"/QC/SE8/*.py")[0], 'w') as file:
             file.write(filedata)
             
@@ -75,47 +73,56 @@ def dna_qc():
             answer = tk.messagebox.askyesno("Confirmation", 'Running QC for SE8 samples')
             if answer:
                 script=glob.glob(location+ '/QC/SE8/' + '*.py')
-                os.system('python3 '+ script[0] )
+                os.system('python3 '+ script )
         else:
-            tk.messagebox.showwarning(title='Warning', message='Please add metrics files to the SE8 folder')
-            answer = tk.messagebox.askyesno("Re-Confirmation", 'Continue with DNA QC for SE8 samples?')
+            tk.messagebox.showwarning(title='Warning', message='Please add metrics files to the folder')
+            answer = tk.messagebox.askyesno("Re-Confirmation", 'Continue with DNA QC?')
             if answer:
-                script=glob.glob(location+ '/QC/SE8/' + '*.py')
-                os.system('python3 '+ script[0] )
+                os.system('python3 '+ script )
             
     ############# performing QC based on CE #######################        
     if len(ce)>0:
         os.system('mkdir '+ location + '/QC/CE')
         os.chdir(location)
+        #copying script to the folder
         for script in glob.glob(script_path+'*CE.py'):
             shutil.copy(script,location + '/QC/CE')
-        for c in ce:
             
+        #copying html files to the respective folder
+        for c in ce:
             shutil.copy(c,location + '/QC/CE')
-        
+            
+        #renaming the files (removing _S1_L001_R1_001_)
+        for i in os.listdir(location+'/QC/CE/'):
+            if '_S1_L001_R1_001_fastqc.html' in i:
+                os.rename(i,i.split('_S1_L001_R1_001_fastqc.html')[0]+".html")
+                
         os.chdir(location+'/QC/CE')
         
         #adding location to the python script
         with open(glob.glob(location+"/QC/CE/*.py")[0], 'r') as file :
             filedata = file.read()
             # Replace the location
-            filedata = filedata.replace('{{location}}', "'" +location + "'")
+            filedata = filedata.replace('{{location}}', "'"+location+"'")
         with open(glob.glob(location+"/QC/CE/*.py")[0], 'w') as file:
             file.write(filedata)
         
         if len(glob.glob(location + '/QC/CE'+'/*metrics*.csv'))>0:
             answer = tk.messagebox.askyesno("Confirmation", 'Running QC for CE samples')
             if answer:
-                script=glob.glob(location+ '/QC/SE8/' + '*.py')
-                os.system('python3 '+ script[0] )
+                os.system('python3 '+ script )
         else:
-            tk.messagebox.showwarning(title='Warning', message='Please add metrics files to the CE folder')
-            answer = tk.messagebox.askyesno("Re-Confirmation", 'Continue with DNA QC for CE samples?')
+            tk.messagebox.showwarning(title='Warning', message='Please add metrics files to the folder')
+            answer = tk.messagebox.askyesno("Re-Confirmation", 'Continue with DNA QC?')
             if answer:
                 script=glob.glob(location+ '/QC/SE8/' + '*.py')
                 os.system('python3 '+ script[0] )
         
-            
 
-            
+        
+
+
+                
+
+                
 
