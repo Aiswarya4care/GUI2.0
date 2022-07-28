6dfc16fade5dee7aed435e9484cb4084cc271ab6
Aiswarya4care <aiswarya@4basecare.com>
2022-07-28 19:07:00 +0530

    cnv changes

--- codes/CNV.py
+++ codes/CNV.py
@@ -46,15 +46,6 @@ def cnv_analysis():
     cnv_capturing_bed= GUIpath + '/bed_files/cnv_bed_files/cnv_capturing_bedfiles/indiegene_whole-gene.bed'
     #ce (indi) and se8(sureselect)
     
-    cnv_intersect_bed_loc= GUIpath + '/bed_files/cnv_bed_files/cnv_intersect_bedfiles/'
-    if test=='TarGT_Indigene':
-        cnv_intersect_bed= cnv_intersect_bed_loc + 'indiegene_whole-gene.bed'
-
-    elif test=='TarGT_Absolute':
-        cnv_intersect_bed= cnv_intersect_bed_loc + 'indiegene_whole-gene.bed'
-    else:
-        cnv_intersect_bed= cnv_intersect_bed_loc + 'cnv_36_genes.bed'
-
     ################# selecting modified capturing bed files #################
     if capturingkit=='SureSelectXT_V8_Covered.bed':
         modified_capt_bed= 'SureSelectXT_V8_Covered.bed'
@@ -77,7 +68,7 @@ def cnv_analysis():
         os.system('chmod 777 *')
 
         #modifying the annotation_mod.sh file
-        cnvconfigfile=location+'/CNV/'+ s + '/samplename_cnv_config.txt'
+        cnvconfigfile=location+'/CNV/'+ s + '/samplename_cnv_config.pl'
         # Read in the file
         with open(cnvconfigfile, 'r') as file :
             filedata = file.read()
@@ -100,21 +91,18 @@ def cnv_analysis():
     f2.close()
     f2= open(cnvrun,"w+")
     f2.write("cd " + location + "/CNV"+ '\n')
+    
     for s in samples:
-
         f2.write("perl CNV_config_Somatic.pl " + projectdir + "/AppResults/"+s+"/Files/"+ s+ ".bam " + GUIpath + "/bed_files/cnv_bed_files/cnv_capturing_bedfiles/"+ modified_capt_bed + " " + s)
-        f2.write('\n')
-        
         f2.write('\n' + controlfreec + " -conf " + s + "/" + s+"_cnv.txt" + '\n')
         f2.write('\n')
         f2.write(bedtools + " -a " +s +"/" + s + ".bam_CNVs" +" -b " + GUIpath + '/bed_files/cnv_bed_files/cnv_capturing_bedfiles/indiegene_whole-gene.bed'  + " -loj | sort -V | awk -F\"\t\" \'{print $1\"\t\"$2\"\t\"$3\"\t\"$4\"\t\"$5\"\t\"$9}\' | awk -vOFS=\"\t\" \'$1=$1; BEGIN { str=\"Chromosome Start End Predicted_copy_number Type_of_alteration Gene\"; split(str,arr,\" \"); for(i in arr) printf(\"%s\t\", arr[i]);print}\' | awk '$6 != \".\"\' > ")
         f2.write(s + "\"_Indiegene_cnv_output.txt\"" + '\n')
         f2.write('\n')
         f2.write(bedtools + " -a " +s +"/" + s + ".bam_CNVs" +" -b " + GUIpath + '/bed_files/cnv_bed_files/cnv_capturing_bedfiles/cnv_36_genes.bed' + " -loj | sort -V | awk -F\"\t\" \'{print $1\"\t\"$2\"\t\"$3\"\t\"$4\"\t\"$5\"\t\"$9}\' | awk -vOFS=\"\t\" \'$1=$1; BEGIN { str=\"Chromosome Start End Predicted_copy_number Type_of_alteration Gene\"; split(str,arr,\" \"); for(i in arr) printf(\"%s\t\", arr[i]);print}\' | awk '$6 != \".\"\' > ")
-        f2.write(s + "\"_cnv_output.txt\"" + '\n')
-        
-        
-        f2.write('\n'+ "##############################")
+        f2.write(s + "\"_cnv_output.txt\"" + '\n')       
+        f2.write('\n'+ "##############################" + '\n')
+
     f2.write('\n'+"echo \"######################\"")
     f2.write('\n'+"echo \"######### DONE #########\"")
     f2.write('\n'+"echo \"######################\"")
