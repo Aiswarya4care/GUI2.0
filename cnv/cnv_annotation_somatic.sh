c222ff412b415a9aed5f329866cbbdc177021ad3
Aiswarya4care <aiswarya@4basecare.com>
2022-08-01 10:06:11 +0530

    cnv bed file access fix

--- cnv/cnv_annotation_somatic.sh
+++ cnv/cnv_annotation_somatic.sh
@@ -13,9 +13,9 @@ echo "##########Starting CNV Control Freec#################"
 echo "##########Ending CNV Control Freec#################"
 #Annotating file
 echo "##########Starting annotation#################"
-{{bedtools}} intersect -a ${line}/${line}.bam_CNVs -b {{GUIpath}}/bed_files/cnv_bed_files/cnv_capturing_bedfiles/indiegene_whole-gene.bed -loj | sort -V | awk -F"\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$9}' | awk -vOFS="\t" '$1=$1; BEGIN { str="Chromosome Start End Predicted_copy_number Type_of_alteration Gene"; split(str,arr," "); for(i in arr) printf("%s\t", arr[i]);print}' | awk '$6 != "."' > ./${line}"_Indiegene_cnv_output.txt"
+{{bedtools}} intersect -a ${line}/${line}.bam_CNVs -b {{GUIpath}}/bed_files/cnv_bed_files/cnv_intersect_bedfiles/indiegene_whole-gene.bed -loj | sort -V | awk -F"\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$9}' | awk -vOFS="\t" '$1=$1; BEGIN { str="Chromosome Start End Predicted_copy_number Type_of_alteration Gene"; split(str,arr," "); for(i in arr) printf("%s\t", arr[i]);print}' | awk '$6 != "."' > ./${line}"_Indiegene_cnv_output.txt"
 
-{{bedtools}} intersect -a ${line}/${line}.bam_CNVs -b {{GUIpath}}/bed_files/cnv_bed_files/cnv_capturing_bedfiles/cnv_36_genes.bed -loj | sort -V | awk -F"\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$9}' | awk -vOFS="\t" '$1=$1; BEGIN { str="Chromosome Start End Predicted_copy_number Type_of_alteration Gene"; split(str,arr," "); for(i in arr) printf("%s\t", arr[i]);print}' | awk '$6 != "."' > ./${line}"_cnv_output.txt"
+{{bedtools}} intersect -a ${line}/${line}.bam_CNVs -b {{GUIpath}}/bed_files/cnv_bed_files/cnv_intersect_bedfiles/cnv_36_genes.bed -loj | sort -V | awk -F"\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$9}' | awk -vOFS="\t" '$1=$1; BEGIN { str="Chromosome Start End Predicted_copy_number Type_of_alteration Gene"; split(str,arr," "); for(i in arr) printf("%s\t", arr[i]);print}' | awk '$6 != "."' > ./${line}"_cnv_output.txt"
 
 echo "##########Ending annotation#################"
 done < "$input"
