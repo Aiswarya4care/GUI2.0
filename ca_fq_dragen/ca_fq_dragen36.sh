2f961a29d824304cf264b5735bc895884611a990
Aiswarya4care <aiswarya@4basecare.com>
2022-08-01 15:11:08 +0530

    dragen script fix

--- ca_fq_dragen/ca_fq_dragen36.sh
+++ ca_fq_dragen/ca_fq_dragen36.sh
@@ -6,8 +6,10 @@ do
    cutadapt -j 30 -m 35 -a {{adapter}} -A {{adapter}} -o ${SAMPLE}_S1_L001_R1_001.fastq.gz -p ${SAMPLE}_S1_L001_R2_001.fastq.gz ${SAMPLE}_R1.fastq.gz ${SAMPLE}_R2.fastq.gz >{{location}}/cutadaptlog/${SAMPLE}_cutadaptlog.txt
 done
 
-sleep 2m
 bs upload dataset --project={{pid}} *R1_001.fastq.gz *R2_001.fastq.gz
+
+sleep 2m
+
 echo "############ FQ and CA completed ###########"
 		
 samples=({{samplenames}})
